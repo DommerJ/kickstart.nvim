fb0496168f1defae6ad95dc0b869646799e2e7db
Jens Gringel <jens.gringel@tu-dresden.de>
2026-04-14 08:12:12 +0200

    ~ depreciated elements

--- lua/custom/plugins/configs/none-ls.lua
+++ lua/custom/plugins/configs/none-ls.lua
@@ -31,14 +31,12 @@ null_ls.setup {
   sources = sources,
   -- you can reuse a shared lspconfig on_attach callback here
   on_attach = function(client, bufnr)
-    if client.supports_method 'textDocument/formatting' then
+    if client:supports_method 'textDocument/formatting' then
       vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
       vim.api.nvim_create_autocmd('BufWritePre', {
         group = augroup,
         buffer = bufnr,
-        callback = function()
-          vim.lsp.buf.format { async = false }
-        end,
+        callback = function() vim.lsp.buf.format { async = false } end,
       })
     end
   end,
